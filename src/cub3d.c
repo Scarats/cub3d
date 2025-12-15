7658277dc3d193af16246f1b0d6ed9708a857069
Thomas.42 <0xscarats@gmail.com>
2025-12-16 00:27:58 +0100

    WIP, there are some issues but i got a first visual.

--- src/cub3d.c
+++ src/cub3d.c
@@ -15,6 +15,7 @@ int	main_logic(t_data *data)
 	if (!data || !data->img_buff || !data->img_main)
 		return (perror("main_logic"), 1);
 	mouse_edge(data);
+	draw_player(data);
 	printf(RED"dir (%f, %f)\n"RESET, data->v_dir.dx, data->v_dir.dy);
 	printf(ORANGE"(%f, %f)\n"RESET, data->position.x, data->position.y);
 	// draw_minimap(data, data->map);
