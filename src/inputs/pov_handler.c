b3b49bc653c935bb8ffde84d0c19ee8c334ab6fc
Thomas.42 <0xscarats@gmail.com>
2025-12-16 01:22:47 +0100

    Working version, just cannot turn the view using the LEFT and RIGHT arrows and moving at the same time.

--- src/inputs/pov_handler.c
+++ src/inputs/pov_handler.c
@@ -4,6 +4,7 @@ void	look_left(t_data *data)
 {
 	data->controls.look_left = true;
 	data->direction_angle = set_angle(data->direction_angle, POV);
+	data->v_dir = get_vector(data->direction_angle);
 }
 
 void	stop_look_left(t_data *data)
@@ -15,6 +16,7 @@ void	look_right(t_data *data)
 {
 	data->controls.look_right = true;
 	data->direction_angle = set_angle(data->direction_angle, -POV);
+	data->v_dir = get_vector(data->direction_angle);
 }
 
 void	stop_look_right(t_data *data)
