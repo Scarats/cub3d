198f349f23f3c3ac1cb99bad11c73abbfe71ae1f
Thomas.home.VM <0xscarats@gmail.com>
2025-12-21 20:07:10 +0100

    refractored version, still WIP

--- src/parser/save_sprites.c
+++ src/parser/save_sprites.c
@@ -1,18 +1,5 @@
 #include "../cub3d.h"
 
-// Malloc the name once and overwrite it everytime.
-char *build_name(char *s1, char *s2, const unsigned int size)
-{
-    char *str;
-
-    if (!s1 || !s2)
-        return (NULL);
-    str = malloc(size + 1);
-    ft_strcpy(str, s1);
-    ft_strcpy((char *)(str + ft_strlen(s1)), s2);
-    return (str);
-}
-
 // Make a function that returns an array of t_img
 t_img **save_sprites(t_data *data, char *path, const unsigned int size)
 {
@@ -24,20 +11,19 @@ t_img **save_sprites(t_data *data, char *path, const unsigned int size)
 
     if (!data || !path || size <= 0)
         return (NULL);
-    number = ft_itoa(10);
     name_len = ft_strlen(path);
     name = ft_strjoin(path, "  ");
+    number = name + name_len;
     i = -1;
     imgs = my_malloc(NULL, &data->malloc_list, sizeof(t_img));
     while (++i < size)
     {
-        ft_strcpy((char *)(name + name_len), ft_itoa_no_malloc(number, i + 1));
+        ft_itoa_no_malloc(number, i + 1);
         imgs[i] = my_malloc(NULL, &data->malloc_list, sizeof(t_img));
         
 
-        ft_memset(name, 0, name_len);
+        ft_memset(number, 0, 2);
     }
-    free(number);
     free(name);
     return (imgs);
 }
