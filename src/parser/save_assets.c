6ab65be0c5c6bf25b58105db8b303ae43ed35d55
Charles <charles.bgnr@gmail.com>
2026-01-21 18:49:43 +0100

    leak fix save_image

--- src/parser/save_assets.c
+++ src/parser/save_assets.c
@@ -21,29 +21,30 @@ void	ft_save_images(t_data *data)
 {
 	data->tex.north.img = mlx_xpm_file_to_image(data->mlx, data->file.north,
 			&data->tex.north.w, &data->tex.north.h);
+	data->tex.south.img = mlx_xpm_file_to_image(data->mlx, data->file.south,
+			&data->tex.south.w, &data->tex.south.h);
+	data->tex.west.img = mlx_xpm_file_to_image(data->mlx, data->file.west,
+			&data->tex.west.w, &data->tex.west.h);
+	data->tex.east.img = mlx_xpm_file_to_image(data->mlx, data->file.east,
+			&data->tex.east.w, &data->tex.east.h);
+	data->tex.door.img = mlx_xpm_file_to_image(data->mlx, data->file.door,
+			&data->tex.door.w, &data->tex.door.h);
+	if (!data->tex.north.img || !data->tex.south.img || !data->tex.west.img
+		|| !data->tex.east.img|| !data->tex.door.img)
+		ft_error(&data, "💥 FT_SAVE_ASSET 💥", 1);
 	data->tex.north.data = (int *)mlx_get_data_addr(data->tex.north.img,
 			&data->tex.north.bpp, &data->tex.north.size,
 			&data->tex.north.endian);
-	data->tex.south.img = mlx_xpm_file_to_image(data->mlx, data->file.south,
-			&data->tex.south.w, &data->tex.south.h);
 	data->tex.south.data = (int *)mlx_get_data_addr(data->tex.south.img,
 			&data->tex.south.bpp, &data->tex.south.size,
 			&data->tex.south.endian);
-	data->tex.west.img = mlx_xpm_file_to_image(data->mlx, data->file.west,
-			&data->tex.west.w, &data->tex.west.h);
 	data->tex.west.data = (int *)mlx_get_data_addr(data->tex.west.img,
 			&data->tex.west.bpp, &data->tex.west.size, &data->tex.west.endian);
-	data->tex.east.img = mlx_xpm_file_to_image(data->mlx, data->file.east,
-			&data->tex.east.w, &data->tex.east.h);
 	data->tex.east.data = (int *)mlx_get_data_addr(data->tex.east.img,
 			&data->tex.east.bpp, &data->tex.east.size, &data->tex.east.endian);
-	data->tex.door.img = mlx_xpm_file_to_image(data->mlx, data->file.door,
-			&data->tex.door.w, &data->tex.door.h);
 	data->tex.door.data = (int *)mlx_get_data_addr(data->tex.door.img,
 			&data->tex.door.bpp, &data->tex.door.size, &data->tex.door.endian);
-	if (!data->tex.north.img || !data->tex.south.img || !data->tex.west.img
-		|| !data->tex.east.img)
-		ft_error(&data, "💥 FT_SAVE_ASSET 💥", 1);
+	
 }
 
 void	ft_save_assets(t_data *data)
